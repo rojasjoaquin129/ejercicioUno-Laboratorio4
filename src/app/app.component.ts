247c4e6897cf319702bc53cac517c639b1c25f6f
joaquin <rojasjoaquin129@gmail.com>
2021-09-08 15:56:54 -0300

    terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Usuario } from './usuario';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ejercicio';
+ 
+  edadUno:string='';
+  edadDos:string='';
+  myUsuario:any;
+  resultado:string='';
+  resultadoSuma:string='';
+  calcular(){
+    this.myUsuario=new Usuario(this.edadUno,this.edadDos);
+    this.resultado=this.myUsuario.Promedio().toString();
+    this.resultadoSuma=this.myUsuario.suma().toString();
+  }
+  limpiar(){
+    this.edadUno='';
+    this.edadDos='';
+    this.myUsuario='';
+    this.resultadoSuma='';
+    this.resultado='';
+  }
 }
